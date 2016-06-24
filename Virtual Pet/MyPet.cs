efef6b24e880e784b9cd0960299b590ad8f7aa2b
Jael <jaele.jones@gmail.com>
2016-06-24 15:16:32 -0400

    test

---
+++ Virtual Pet/MyPet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Pet
+{
+    public class MyPet // These are my methods?
+    {
+        private string name { get; set; }
+        public int feedpts { get; set}
+        public int playpts { get; set}
+        public int sleeppts { get; set}
+        public int pottypts { get; set}
+
+        //I might had something for pooping but what? points for pooping or just use a method to have to pet poop.
+    }
+}
--- Virtual Pet/Program.cs
+++ Virtual Pet/Program.cs
@@ -7,11 +7,33 @@ using System.Threading.Tasks;
 namespace Virtual_Pet
 {
     class Program
+    {
+        /*
+         * Message from Pet
+         *Directions for owner
 
-    
+         */
 
         static void Main(string[] args)
-        {
+        {        /*   Console.WriteLine();
+                  Console.WriteLine();
+                  Console.WriteLine();
+                  Console.WriteLine("Hello, oh you are my owner!");
+                  Console.WriteLine("Will you love me and take care fo me?");
+                  Console.WriteLine("Im so lucky to have you!");
+                  Console.WriteLine("Directions for owner, (e.i ,if you want to feed me press 1, and so forth)
+                  Console.WriteLine("1.Let'play")//Or I'll make a mess
+                  Console.WriteLine("2.I'm hungry in my tummy");
+                  Console.WriteLine("3.Im so sleepy");
+ -                Console.WriteLine("4. I need to potty,speedy!);
+ -                Console.WriteLine("5. Exit Menu");
+ +                Console.WriteLine();
+                  Console.WriteLine();
+                 
+                  //print out the menu
+ -                int choice = Convert.ToInt32(Console.ReadLine()); */
         }
     }
 }
+
+
