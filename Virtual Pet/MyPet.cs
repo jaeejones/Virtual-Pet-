266d30c28865178451122ad81296c16563aa293e
Jael <jaele.jones@gmail.com>
2016-06-24 16:32:23 -0400

    New Change

--- Virtual Pet/MyPet.cs
+++ Virtual Pet/MyPet.cs
@@ -8,11 +8,11 @@ namespace Virtual_Pet
 {
     public class MyPet // These are my methods?
     {
-        private string name { get; set; }
-        public int feedpts { get; set}
-        public int playpts { get; set}
-        public int sleeppts { get; set}
-        public int pottypts { get; set}
+        private string name { get; private set; }
+        public int  HungerLevel { get; set}
+        public int  PetMood{ get;  private set}
+        public int sleeppts { get; private set}
+        public int pottypts { get;  private set}
 
         //I might had something for pooping but what? points for pooping or just use a method to have to pet poop.
     }
--- Virtual Pet/Program.cs
+++ Virtual Pet/Program.cs
@@ -15,25 +15,61 @@ namespace Virtual_Pet
          */
 
         static void Main(string[] args)
-        {        /*   Console.WriteLine();
-                  Console.WriteLine();
-                  Console.WriteLine();
-                  Console.WriteLine("Hello, oh you are my owner!");
-                  Console.WriteLine("Will you love me and take care fo me?");
-                  Console.WriteLine("Im so lucky to have you!");
-                  Console.WriteLine("Directions for owner, (e.i ,if you want to feed me press 1, and so forth)
-                  Console.WriteLine("1.Let'play")//Or I'll make a mess
-                  Console.WriteLine("2.I'm hungry in my tummy");
-                  Console.WriteLine("3.Im so sleepy");
- -                Console.WriteLine("4. I need to potty,speedy!);
- -                Console.WriteLine("5. Exit Menu");
- +                Console.WriteLine();
-                  Console.WriteLine();
-                 
-                  //print out the menu
- -                int choice = Convert.ToInt32(Console.ReadLine()); */
+        {
+            while (true)
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Hello, oh you are my owner!");
+            Console.WriteLine("Will you love me and take care fo me?");
+            Console.WriteLine("Im so lucky to have you!");
+            Console.WriteLine("Directions for owner,e.i ,if you want to feed me press 1, and so forth");
+            Console.WriteLine("1.Let'play");//Or I'll make a mess
+
+            Console.WriteLine("2.I'm hungry in my tummy");
+            Console.WriteLine("3.Im so sleepy");
+            Console.WriteLine("4.I need to potty,speedy!");
+
+            Console.WriteLine("5. Uh-oh, I don't feel so good!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            //print out the menu
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+
+
+            {
+                Playpts(); //initiate the method to play with the pet and earn points 
+
+
+            }
+            else if (choice == 2)
+            {
+                Feedpts(); //initiate the method to feed the pet and earn points 
+
+            }
+            else if (choice == 3)
+            {
+                Sleeppts();//initiate the method to rest the pet and earn points 
+            }
+            else if (choice == 4)
+            {
+                Pottypts(); //initiate the method to rest the pet and earn points 
+
+            }
+            else if (choice == 5)
+            {
+
+            }
+            else
+            {
+                //Unknown choice!
+                Console.WriteLine("Pick 1-5 to help me stay alive");
+            }
         }
     }
 }
-
 
