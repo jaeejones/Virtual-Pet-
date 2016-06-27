a99781d20b7e6d5f10f4d665b0eb1fd1e048b6a8
Jael <jaele.jones@gmail.com>
2016-06-27 18:02:45 -0400

    "Its not working very well!"

--- Virtual Pet/MyPet.cs
+++ Virtual Pet/MyPet.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace VirtualPet
 {
-    public class VirtualPet // These are properties? or mymojo how do I set the name
+    public class VirtualPet 
+
     {
         private int _play;
-        public int Playlevel
+        public int PlayLevel
         {
             get
             {
@@ -61,19 +62,79 @@ namespace VirtualPet
             }
         }
 
-        public void PlayLevelpt()
+        public VirtualPet()
+        {
+            PlayLevel = 11;
+            HungerLevel = 9;
+            SleepLevel = 7;
+            PoopLevel = 5;
+           
+        }
+
+        public void DisplayMenu()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("\n Here are your choices!\n");
+            Console.WriteLine("\n Do you have the brains to choose intellegently\n");
+            Console.WriteLine("1.Play,NOW");
+            Console.WriteLine("2.Feed me!");
+            Console.WriteLine("3.Put me to sleep");
+            Console.WriteLine("4.Take me out to poop");
+            Console.WriteLine("5.Leave, out of my sight!"); //Exit
+
+
+            Console.WriteLine("\n Choose from the numbers above, genius!");
+
+            int menuChoice = Convert.ToInt32(Console.ReadLine());
+
+
+            while (menuChoice != 5)
+            {
+                if (menuChoice == 1)
+                {
+
+                    PlayLevelpt();
+                }
+                else if (menuChoice == 2)
+                {
+                    HungerLevelpt();
+                }
+                else if (menuChoice == 3)
+                {
+                    SleepLevelpt();
+                }
+                else if (menuChoice == 4)
+                {
+                    PoopLevelpt();
+                }
+
+                else
+                {
+
+                    Console.WriteLine("\n You are most honored to take care of me.\n");
+                    Console.WriteLine("To Exit press any key smarty!");
+
+
+
+                }
+
+
+
+            }
+            Console.ReadKey();
+        }
+        
+    public void PlayLevelpt()
 
         {
 
             Console.WriteLine("Mo Jojo is needs amuzement,Play with me now!");
-            int fun = int.Parse(Console.ReadLine());
+            //int fun = int.Parse(Console.ReadLine());
 
-            if (fun == 2)
-            {
-                _play = _play - 11;
-                Console.WriteLine("I have been appropriately entertained, now GO!");
-
-            }
+            //if (fun == 2)
+            
+               PlayLevel = PlayLevel- 11;
 
             DisplayPetStatus();
         }
@@ -82,14 +143,10 @@ namespace VirtualPet
         {
 
             Console.WriteLine("Mo Jojo is hungry! Feed me, NOW\n");
-            int bananas = int.Parse(Console.ReadLine());
-            if (bananas == 2)
-            {
-                _hunger = _hunger - 9;
-
-                Console.WriteLine("You have Satified Me!\n");
-
-            }
+            //int bananas = int.Parse(Console.ReadLine());
+            //if (bananas == 2)
+            
+                HungerLevel = HungerLevel- 9;
 
             DisplayPetStatus();
         }
@@ -98,14 +155,10 @@ namespace VirtualPet
         {
 
             Console.WriteLine(" Mo Jojo is weary of you, I need a nap!");
-            int rest = int.Parse(Console.ReadLine());
+            //int rest = int.Parse(Console.ReadLine());
 
-            if (rest == 1)
-            {
-                _sleep = _sleep - 7;
-                Console.WriteLine("ZZZZZZZZZ!");
-
-            }
+            //if (rest == 1)
+                
 
             DisplayPetStatus();
         }
@@ -115,13 +168,11 @@ namespace VirtualPet
 
 
             Console.WriteLine("Mo Jojo will use your toliet to poop!");
-            int _poop = int.Parse(Console.ReadLine());
-            if (_poop == 1)
-            {
-                _poop = _poop - 5;
-
-                Console.WriteLine("Now you must clean your toliet, heheh!");
-            }
+           // int _poop = int.Parse(Console.ReadLine());
+            //if (_poop == 1)
+            
+                PoopLevel = PoopLevel - 5;
+            
 
 
             DisplayPetStatus();
@@ -134,68 +185,73 @@ namespace VirtualPet
 
             Console.WriteLine("=======================");
             Console.WriteLine();
-            Console.WriteLine("\nPlay {0} \n" + "Food: {1}\n" + "Sleep: {2}\n" +
-                              "Poop: {3}\n" + _play, _hunger, _sleep, _poop);
+            /*Console.WriteLine("\nPlay {0} \n" + "Food: {1}\n" + "Sleep: {2}\n" +
+                              "Poop: {3}\n" + PlayLevel, SleepLevel, HungerLevel, PoopLevel);*/
 
             Console.WriteLine();
             Console.WriteLine("========================");
 
-            {
-                if (_play >= 11 && _play < 11)
+
+            if  (PlayLevel >= 11 && PlayLevel < 11)
                 {
                     Console.WriteLine("Play with me!");
                 }
-                if (_play >= 10)
+                if (PlayLevel >= 10)
                 {
                     Console.WriteLine("You will punshied for your lack of fun!");
 
                 }
-                if (_hunger >= 9 && _hunger < 9)
+                if (HungerLevel >= 9 && HungerLevel < 9)
                 {
                     Console.WriteLine("Feed Me!");
                 }
-                if (_hunger >= 100)
+                if (HungerLevel >= 100)
                 {
                     Console.WriteLine("I will now distroy your kitchen for food!/n");
 
                 }
-                if (_sleep >= 7 && _sleep < 7)
+                if (SleepLevel >= 7 && SleepLevel< 7)
                 {
                     Console.WriteLine("I am sleepy!\n");
                 }
-                if (_sleep >= 100)
+                if (SleepLevel >= 100)
                 {
                     Console.WriteLine("YOUR PET HAS LEFT YOU FOR SOMEONE MORE FUN!");
 
                 }
 
-                if (_play < 0)
+                if (PlayLevel < 0)
                 {
-                    _play = 0;
+                    PlayLevel = 0;
                 }
 
-                if (_hunger < 0)
+                if (HungerLevel< 0)
                 {
-                    _hunger = 0;
+                    HungerLevel = 0;
                 }
 
-                if (_sleep < 0)
+                if (SleepLevel < 0)
                 {
-                    _sleep = 0;
+                    SleepLevel= 0;
                 }
-                if (_poop < 0)
+                if (PoopLevel < 0)
                 {
-                    _poop = 0;
-          
+                    PoopLevel = 0;
+
                 }
                 Console.ReadKey();
             }
-            
+
         }
 
-        
-    }
-}
+    } 
+
+
+
+
+
+
+
 
 // 6/25/16 I beleive I have created all of my methods. I am still confused on what operates where. Do, I set my methods in the program.cs doc or t
 
--- Virtual Pet/Program.cs
+++ Virtual Pet/Program.cs
@@ -11,11 +11,15 @@ namespace VirtualPet
     {
         //private static readonly object mymojo;
 
+        static VirtualPet MyMojo = new VirtualPet();
+
         static void Main(string[] args)
         {
+            MyMojo.DisplayMenu();
 
 
-            Console.WriteLine("************************************");
+
+            /*Console.WriteLine("************************************");
             Console.WriteLine();
             Console.WriteLine("\n I am Mojojo an intelgent monkey!\n");
             Console.WriteLine("\n You must be very furtunate to have me as your pet!/n");
@@ -27,60 +31,10 @@ namespace VirtualPet
 
             Console.WriteLine("\n Than get to work,now! The instructions are below!/n");
             Console.WriteLine();
-            Console.WriteLine("*************************************");
-        }
-        public static void DisplayMenu()
-        {
-            VirtualPet mymojo = new VirtualPet();
-            Console.WriteLine();
-            Console.WriteLine("\n Here are your choices!/n");
-            Console.WriteLine("\n Do you have the brains to choose intellegently/n");
-            Console.WriteLine("1.Play,NOW");
-            Console.WriteLine("2.Feed me!");
-            Console.WriteLine("3.Put me to sleep");
-            Console.WriteLine("4.Take me out to poop");
-            Console.WriteLine("5.Leave, out of my sight!"); //Exit
-            Console.WriteLine("\n Choose from the numbers above, genius!");
-            int menuChoice = Convert.ToInt32(Console.ReadLine());
-
-            //return menuChoice;
+            Console.WriteLine("*************************************");*/
 
 
-
-            while (menuChoice != 5)
-            {
-                if (menuChoice == 1)
-                {
-
-                    mymojo.PlayLevelpt(); //
-                }
-                else if (menuChoice == 2)
-                {
-                    mymojo.HungerLevelpt();
-                }
-                else if (menuChoice == 3)
-                {
-                    mymojo.SleepLevelpt();
-                }
-                else if (menuChoice == 4)
-                {
-                    mymojo.PoopLevelpt();
-                }
-
-                else
-                {
-
-                    Console.WriteLine("\n You are most honored to take care of me.\n");
-                    Console.WriteLine("To Exit press any key smarty!");
-                    
-
-                   
-                }
-               
-
-
-        }
-            Console.ReadKey();
         }
     }
+
 }
